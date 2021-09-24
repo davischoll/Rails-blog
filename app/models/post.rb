c2c97262bba9cf3d2906d06988c1edcf0afcc9f4
Davi Scholl <davis@bandavide.com.br>
2021-09-23 23:42:43 -0300

    added slug configuration with gem friendly id

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,14 @@
 class Post < ApplicationRecord
+  include PgSearch::Model
+  extend FriendlyId
+
   validates :title, :author, :body, presence: true
 
   has_many :comments
+
+  friendly_id :title, use: :slugged
+
+  pg_search_scope :search,
+    against: %i[title author body],
+    associated_against: { comments: %i[body] }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_17_021921) do
+ActiveRecord::Schema.define(version: 2021_09_24_020545) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,8 @@ ActiveRecord::Schema.define(version: 2021_09_17_021921) do
     t.text "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "slug"
+    t.index ["slug"], name: "index_posts_on_slug", unique: true
   end
 
   add_foreign_key "comments", "posts"
